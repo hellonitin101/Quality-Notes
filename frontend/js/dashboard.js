05551135c51930cbaf8a511e61eb4f67ef6a9d33
Nitin Singh Shekhawat <hellonitin101@gmail.com>
2026-05-21 15:45:59 +0530

    Connected frontend with deployed backend

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,20 +1,27 @@
 const user = JSON.parse(localStorage.getItem("user"));
 
 if(!user){
+
     window.location.href = "login.html";
+
 }
 
 document.getElementById("welcomeText").innerText =
 `Welcome, ${user.name}`;
 
 document.getElementById("studentName").value = user.name;
+
 document.getElementById("studentEmail").value = user.email;
 
 
+
+
 // LOGOUT
+
 function logout(){
 
     localStorage.removeItem("token");
+
     localStorage.removeItem("user");
 
     window.location.href = "login.html";
@@ -23,8 +30,36 @@ function logout(){
 
 
 
+
+// NOTES TYPE CUSTOM INPUT
+
+const notesType =
+document.getElementById("notesType");
+
+const customNotesType =
+document.getElementById("customNotesType");
+
+notesType.addEventListener("change",()=>{
+
+    if(notesType.value === "Other"){
+
+        customNotesType.style.display = "block";
+
+    }else{
+
+        customNotesType.style.display = "none";
+
+    }
+
+});
+
+
+
+
 // REQUEST FORM
-const requestForm = document.getElementById("requestForm");
+
+const requestForm =
+document.getElementById("requestForm");
 
 requestForm.addEventListener("submit",(e)=>{
 
@@ -32,29 +67,51 @@ requestForm.addEventListener("submit",(e)=>{
 
     const requestData = {
 
-        studentName:document.getElementById("studentName").value,
+        studentName:
+        document.getElementById("studentName").value,
 
-        studentEmail:document.getElementById("studentEmail").value,
+        studentEmail:
+        document.getElementById("studentEmail").value,
 
-        course:document.getElementById("course").value,
+        course:
+        document.getElementById("course").value,
 
-        subject:document.getElementById("subject").value,
+        subject:
+        document.getElementById("subject").value,
 
-        notesType:document.getElementById("notesType").value,
+        notesType:
 
-        language:document.getElementById("language").value,
+        document.getElementById("notesType").value === "Other"
 
-        deadline:document.getElementById("deadline").value,
+        ? document.getElementById("customNotesType").value
 
-        syllabus:document.getElementById("syllabus").value,
+        : document.getElementById("notesType").value,
 
-        extraInfo:document.getElementById("extraInfo").value
+        language:
+
+document.getElementById("language").value === "Other"
+
+? document.getElementById("customLanguage").value
+
+: document.getElementById("language").value,
+
+        deadline:
+        document.getElementById("deadline").value,
+
+        syllabus:
+        document.getElementById("syllabus").value,
+
+        extraInfo:
+        document.getElementById("extraInfo").value
 
     };
 
     localStorage.setItem(
+
         "requestData",
+
         JSON.stringify(requestData)
+
     );
 
     window.location.href = "payment.html";
